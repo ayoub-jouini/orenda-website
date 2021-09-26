ffb2c5e3ebdf0cb575565fcde5089d8de77d8762
ayoub_jouini <ayoubjouini135@gmail.com>
2021-09-26 15:35:54 +0100

    adding style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Header from './components/Header/header';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './style/scss/style.scss'
+import './style/css/style.css'
+
+
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
     </div>
   );
 }
