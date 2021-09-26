94bfc9dfdb8d10fb59bd1a53eefa0d7274bdb65a
ayoub_jouini <ayoubjouini135@gmail.com>
2021-09-26 15:35:36 +0100

    adding the error boundary to the app

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ErrorBoundary from './components/Error Boundary/ErrorBoundary';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root')
 );
