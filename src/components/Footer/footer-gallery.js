8cfcb53def5115314e2e0e6cd2f8d55742aa9798
ayoub_jouini <ayoubjouini135@gmail.com>
2021-09-27 19:52:57 +0100

    FooterGallery component

---
+++ src/components/Footer/footer-gallery.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const FooterGallery = () => {
+    return (
+        <div className="footer-gallery">
+            <div className="row">
+                <div className="col-4">
+                    <a href="img/bg-img/21.jpg" className="single-gallery-item"><img src="./assets/images/bg-img/21.jpg" alt="" /></a>
+                </div>
+                <div className="col-4">
+                    <a href="img/bg-img/22.jpg" className="single-gallery-item"><img src="./assets/images/bg-img/22.jpg" alt="" /></a>
+                </div>
+                <div className="col-4">
+                    <a href="img/bg-img/23.jpg" className="single-gallery-item"><img src="./assets/images/bg-img/23.jpg" alt="" /></a>
+                </div>
+                <div className="col-4">
+                    <a href="img/bg-img/24.jpg" className="single-gallery-item"><img src="./assets/images/bg-img/24.jpg" alt="" /></a>
+                </div>
+                <div className="col-4">
+                    <a href="img/bg-img/25.jpg" className="single-gallery-item"><img src="./assets/images/bg-img/25.jpg" alt="" /></a>
+                </div>
+                <div className="col-4">
+                    <a href="img/bg-img/26.jpg" className="single-gallery-item"><img src="./assets/images/bg-img/26.jpg" alt="" /></a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default FooterGallery;
