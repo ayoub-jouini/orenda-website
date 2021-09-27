03c9e45ce0f8fc57370eff6309c5d43ed1c3ce2a
ayoub_jouini <ayoubjouini135@gmail.com>
2021-09-27 19:52:36 +0100

    FooterContact component

---
+++ src/components/Footer/footer-contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const FooterContact = () => {
+    return (
+        <div className="footer-contact-info">
+            <p><i className="zmdi zmdi-map"></i>University campus of manoubap</p>
+            <p><i className="zmdi zmdi-phone"></i> (+216) 54 521 658</p>
+            <p><i className="zmdi zmdi-email"></i> Orendajunior.entreprise@gmail.com</p>
+
+        </div>
+    );
+}
+
+export default FooterContact;
