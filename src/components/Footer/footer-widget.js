91515196639ab291f275d674a2b34b983eead8a1
ayoub_jouini <ayoubjouini135@gmail.com>
2021-09-27 19:59:21 +0100

    FooterWidget component

--- src/components/Footer/footer-widget.js
+++ src/components/Footer/footer-widget.js
@@ -1,37 +1,19 @@
 import React from "react";
 
-import FooterContact from './footer-contact';
-import FooterGallery from './footer-gallery';
-import FooterNav from './footer-nav';
 
-const FooterWidget = () => {
-
-    const widgets = [{
-        widgetBody: <FooterContact />,
-        title: "Contact"
-    }, {
-        widgetBody: < FooterNav />,
-        title: "Services"
-    }, {
-        widgetBody: < FooterGallery />,
-        title: "Gallery"
-    }];
+const FooterWidget = ({ title, widgetBody }) => {
 
     return (
 
-        widgets.map((widget, key) => {
-            return (
-                <div className="col-12 col-sm-6 col-lg-3" key={key}>
-                    <div className="single-footer-widget mb-60 wow fadeInUp" data-wow-delay="300ms">
+        <div className="col-12 col-sm-6 col-lg-3">
+            <div className="single-footer-widget mb-60 wow fadeInUp" data-wow-delay="300ms">
 
-                        <h5 className="widget-title">{widget.title}</h5>
+                <h5 className="widget-title">{title}</h5>
 
-                        {widget.widgetBody}
-                    </div>
-                </div>
-            )
+                {widgetBody}
+            </div>
+        </div>
 
-        })
 
     );
 }
