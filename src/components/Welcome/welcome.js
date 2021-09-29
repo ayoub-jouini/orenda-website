11e7f8126bbcc3c6df09bfd32259bebaf9567ed9
ayoub_jouini <ayoubjouini135@gmail.com>
2021-09-30 00:12:35 +0100

    welcome component

--- src/components/Welcome/welcome.js
+++ src/components/Welcome/welcome.js
@@ -1,43 +1,90 @@
-import React from "react";
-import Slide from "./slide";
+import React, { useState } from "react";
+import {
+    Carousel,
+    CarouselItem,
+    CarouselControl,
+    CarouselCaption
+} from 'reactstrap';
 
+
+const Slides = [
+    {
+        firstWord: "Video games ",
+        secondWord: "Developpement",
+        image: "./assets/images/bg-img/1.jpg"
+    },
+    {
+        firstWord: "Graphic ",
+        secondWord: "Design",
+        image: "./assets/images/bg-img/45.jpg"
+    },
+    {
+        firstWord: "Web",
+        secondWord: "Developpement",
+        image: "./assets/images/bg-img/46.jpg"
+    },
+    {
+        firstWord: "Video ",
+        secondWord: "Editing",
+        image: "./assets/images/bg-img/47.jpg"
+    }
+];
 const Welcome = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [animating, setAnimating] = useState(false);
 
-    const Slides = [
-        {
-            firstWord: "Video games ",
-            secondWord: "Developpement",
-            image: "./assets/images/bg-img/1.jpg"
-        },
-        {
-            firstWord: "Graphic ",
-            secondWord: "Design",
-            image: "./assets/images/bg-img/45.jpg"
-        },
-        {
-            firstWord: "Web",
-            secondWord: "Developpement",
-            image: "./assets/images/bg-img/46.jpg"
-        },
-        {
-            firstWord: "Video ",
-            secondWord: "Editing",
-            image: "./assets/images/bg-img/47.jpg"
-        }
-    ];
+    const next = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === Slides.length - 1 ? 0 : activeIndex + 1;
+        setActiveIndex(nextIndex);
+    }
+
+    const previous = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === 0 ? Slides.length - 1 : activeIndex - 1;
+        setActiveIndex(nextIndex);
+    }
+
+    const slides = Slides.map((item) => {
+        return (
+            <CarouselItem
+                onExiting={() => setAnimating(true)}
+                onExited={() => setAnimating(false)}
+                key={item.src}
+            >
+                <div className="single-welcome-slide bg-img bg-overla jarallax" style={{ backgroundImage: 'url(' + item.image + ')' }
+                }>
+                    <div className="container h-100">
+                        <div className="row h-100 align-items-center">
+
+                            <div className="col-12">
+                                <div className="welcome-text text-right">
+                                    <h2 data-animation="fadeInUp" data-delay="300ms">{item.firstWord}<br />{item.secondWord}</h2>
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div >
+                <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+            </CarouselItem>
+        );
+    });
 
     return (
         <section className="welcome-area">
-            <div className="welcome-slides owl-carousel">
-                {Slides.map((slide, key) => {
-                    return (
-                        <Slide key={key} firstWord={slide.firstWord} secondWord={slide.secondWord} image={slide.image} />
-                    );
-                })}
+            <Carousel
+                activeIndex={activeIndex}
+                next={next}
+                previous={previous}
+                className="welcome-slides"
+            >
 
-            </div>
-
-
+                {slides}
+                <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+                <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+            </Carousel>
             <div className="icon-scroll" id="scrollDown"></div>
         </section>
     );
