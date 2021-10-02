cd0b16e877d22563e8f3f4ca4674b82eb953e403
ayoub_jouini <ayoubjouini135@gmail.com>
2021-10-02 22:43:02 +0100

    adding sponsors & contact

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,7 +1,9 @@
 import React from "react";
 import AboutUs from "../components/About/about";
 import Blogs from "../components/Blogs/blogs";
+import Contact from "../components/Contact/contact";
 import Schedule from "../components/Schedule/schedule";
+import Sponsors from "../components/Sponsors/sponsors";
 import Team from "../components/Team/team";
 import Welcome from "../components/Welcome/welcome";
 
@@ -13,6 +15,8 @@ const HomePgae = () => {
             <Team />
             <Schedule />
             <Blogs />
+            <Sponsors />
+            <Contact />
         </>
     );
 
