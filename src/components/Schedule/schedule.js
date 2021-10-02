a78e0eb11f9970e3b1545d99d2951604d2aaf608
ayoub_jouini <ayoubjouini135@gmail.com>
2021-10-02 20:17:32 +0100

    Schedule component

---
+++ src/components/Schedule/schedule.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import ScheduleTab from './schedule-tab';
+
+const events = [
+    {
+        tabName: "Megatron 6.0",
+        about: "About artificial intelligence",
+        img: "10.jpg",
+        date: "18 April 2019",
+        place: "ISAMM"
+    },
+    {
+        tabName: "Launch The Impact",
+        about: "Aboout entrepreneurship",
+        img: "11.jpg",
+        date: " 24 November 2019",
+        place: "ISAMM"
+    }
+]
+
+const Schedule = () => {
+    return (
+        <section className="our-schedule-area section-padding-100" id="event">
+            <div className="container">
+                <div className="row">
+
+                    <div className="col-12">
+                        <div className="section-heading-2 text-center wow fadeInUp" data-wow-delay="300ms">
+                            <p>About our events</p>
+                            <h4>Our Events </h4>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-12">
+                        <div className="schedule-tab">
+
+
+                        </div>
+
+
+                        <div className="tab-content" id="conferScheduleTabContent">
+                            <div className="tab-pane fade show active" id="step-one" role="tabpanel" aria-labelledby="monday-tab">
+
+                                <div className="single-tab-content">
+                                    <div className="row">
+                                        <div className="col-12">
+
+                                            {
+                                                events.map((event, key) => {
+                                                    return (
+                                                        <ScheduleTab key={key}
+                                                            tabName={event.tabName}
+                                                            about={event.about}
+                                                            img={event.img}
+                                                            date={event.date}
+                                                            place={event.place} />
+                                                    )
+                                                })
+                                            }
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </section>
+    );
+}
+
+export default Schedule;
