d687711ac6490562b7ab148c8fa6aaff0b7c0906
ayoub_jouini <ayoubjouini135@gmail.com>
2021-10-02 22:41:54 +0100

    ContactInfo component

---
+++ src/components/Contact/contact-info.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const ContactInfo = () => {
+    return (
+        <div class="contact-information mb-100">
+
+            <div class="single-contact-info wow fadeInUp" data-wow-delay="300ms">
+                <p>Address:</p>
+                <h6>University campus of manouba, ISAMM</h6>
+            </div>
+
+            <div class="single-contact-info wow fadeInUp" data-wow-delay="300ms">
+                <p>Phone:</p>
+                <h6>(+216) 54 521 658</h6>
+            </div>
+
+            <div class="single-contact-info wow fadeInUp" data-wow-delay="300ms">
+                <p>Email:</p>
+                <h6>Orendajunior.entreprise@gmail.com</h6>
+            </div>
+
+        </div>
+    );
+}
+
+export default ContactInfo;
