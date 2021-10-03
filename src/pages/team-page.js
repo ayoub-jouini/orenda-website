8b2ba5656251f6c757f0d5feecf739768e59373e
ayoub_jouini <ayoubjouini135@gmail.com>
2021-10-03 16:04:33 +0100

    adding executiveBoard component

--- src/pages/team-page.js
+++ src/pages/team-page.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Breadcrumb } from "reactstrap";
+import ExecutiveBoard from "../components/Executive Board/executiveBoard";
 
 const TeamPage = () => {
     return (
-        <div>
+        <>
+            <Breadcrumb title="Our Team" />
+            <ExecutiveBoard />
 
-        </div>
+        </>
     );
 
 }
