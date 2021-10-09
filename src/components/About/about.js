2b492c9588a03548661af0bcf479ba7f6a87155b
ayoub_jouini <ayoubjouini135@gmail.com>
2021-10-09 23:01:58 +0100

    About animation

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import { useAnimation, motion } from 'framer-motion';
 
 import AboutContent from './about-content';
 import AboutThumb from './about-thumb';
@@ -6,14 +8,40 @@ import CounterUpArea from './counterUp-area';
 
 
 const AboutUs = () => {
+
+    const [ref, inView] = useInView({
+        threshold: 0.2
+    });
+    const animation = useAnimation();
+    useEffect(() => {
+        if (inView) {
+            animation.start({
+                opacity: 1,
+                y: 0,
+                transition: {
+                    type: "spring",
+                    duration: 1.5
+                }
+            })
+        }
+        if (!inView) {
+            animation.start({
+                y: -100,
+                opacity: 0
+            })
+        }
+
+    }, [inView])
+
     return (
-        <section className="about-us-countdown-area section-padding-100-0" id="about">
+        <section ref={ref} className="about-us-countdown-area section-padding-100-0" id="about">
             <div className="container">
-                <div className="row align-items-center">
+                <motion.div className="row align-items-center"
+                    animate={animation}>
                     <AboutContent />
 
                     <AboutThumb />
-                </div>
+                </motion.div>
             </div>
 
             <CounterUpArea />
