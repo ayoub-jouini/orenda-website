14384ffe7b8b0224f17615262876248d22e3f500
ayoub_jouini <ayoubjouini135@gmail.com>
2021-10-26 19:22:05 +0100

    mail.php

--- php/mail.php
+++ php/mail.php
@@ -6,29 +6,35 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
+
+header("Access-Control-Allow-Origin: *");
+$rest_json = file_get_contents("php://input");
+$_POST = json_decode($rest_json, true);
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
-
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
+    $mail->isSMTP();
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                     //Enable verbose debug output                                         //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->SMTPAuth   = true;
+    $mail->SMTPSecure = 'ssl';                                 //Enable SMTP authentication
     $mail->Username   = '';                                 //SMTP username
     $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->Port       = 465;
+    $mail->SMTPAutoTLS = false;                                  //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
     $mail->setFrom($_POST["email"], $_POST["name"]); //"Sender email", "Sender username"
-    $mail->addAddress('ayoubjouini135@gmail.com');     //To: ...
+    $mail->addAddress('');     //To: ...
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = "contact";
+    $mail->Body    = $_POST["message"];
     $mail->AltBody = $_POST["message"];
 
     $mail->send();
